4371983461a1b578df0bcf4b8fd78b60eadb6d33
Juan David Yepes <judayego@outlook.com>
2020-10-27 07:27:38 -0500

    Base Proyecto

---
+++ src/webparts/proyectos/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IProyectosWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ProyectosWebPartStrings' {
+  const strings: IProyectosWebPartStrings;
+  export = strings;
+}
